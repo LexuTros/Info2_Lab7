88bdaa6593c93fc98fefafdd4df81f3f2f0b53d2
LexuTros <lennart@toellke.com>
2022-04-27 11:33:32 +0200

    just started

---
+++ task2.c
@@ -0,0 +1,24 @@
+/****************************************************************************
+ * University of Zurich, Informatics II, Spring Semester 2022               *
+ * Exercise 7 - Task 2                                                      *
+ *                                                                          *
+ * @author LexuTros                                                         *
+ ****************************************************************************/
+# include <stdio.h>	
+# include <stdlib.h>
+
+
+typedef struct Stack {  
+	unsigned int capacity; // positive int (with 0)
+	int* items; 
+  int top; // idx of first free element of stack
+} Stack;
+
+void create(Stack S){
+	
+}
+
+int main() {
+	//TODO: your implementation
+  return 0;
+}
